7171b67d4052d13f622748ae087706d3f87c1d02
JacobJershin <70407880+JacobJershin@users.noreply.github.com>
2022-05-09 04:40:03 -0500

    Create PlayerClears.js

---
+++ TheTop150/appScript/PlayerClears.js
@@ -0,0 +1,25 @@
+function PlayerClears () {
+  Counter = 0
+  LevelID = 0
+  index = 0
+  PlayerID = 0
+  while (LevelID < 130) {
+    while (PlayerID < PlayerNameArray.length) {
+      while (index < MasterClears[PlayerID].length) {
+        if (MasterClears[PlayerID][index] == LevelID) {
+        ListPage.getRange(LevelID, Counter + 7).setValue(PlayerNameArray[PlayerID])
+        Counter++
+      }
+        index++
+      }
+      index = 0
+      PlayerID++
+  }
+    PlayerID = 0
+    index = 0
+    Counter = 0
+    LevelID++
+  }
+}
+
+console.log("Finished the level clears execution")
