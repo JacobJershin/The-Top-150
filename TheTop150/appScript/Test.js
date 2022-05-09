8731cac432fafab64d5752b5507e20f3973f39fe
JacobJershin <70407880+JacobJershin@users.noreply.github.com>
2022-05-09 04:41:02 -0500

    Create Test.js

---
+++ TheTop150/appScript/Test.js
@@ -0,0 +1,7 @@
+function WhatToRun() {
+  ProfilePageCode(69) //which player ID to display
+  PlayerClears()
+  LeaderBoard()
+  PlayerUploads()
+  LevelHistory(2000) //Day it was after smm2s release
+}
